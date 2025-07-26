23f6d2dfe6c6b092f637f1f1d7b54dac7505b5ce
Oualid <jeridi@et.esiea.fr>
2025-07-26 15:13:37 +0200

    Update creation_des_tables.sql

--- sql/creation_des_tables.sql
+++ sql/creation_des_tables.sql
@@ -1,1 +1,32 @@
+CREATE TABLE Clients (
+  ID NUMBER PRIMARY KEY,
+  Nom VARCHAR2(50),
+  Prenom VARCHAR2(50),
+  DateNaissance DATE
+);
 
+CREATE TABLE Médicaments (
+  ID NUMBER PRIMARY KEY,
+  Nom VARCHAR2(50),
+  Prix NUMBER(10,2),
+  Stock NUMBER,
+  NecessiteOrdonnance CHAR(1) -- 'O' ou 'N'
+);
+
+
+CREATE TABLE Ventes (
+  ID NUMBER PRIMARY KEY,
+  ID_Client NUMBER,
+  DateVente DATE,
+  MontantTotal NUMBER(10,2),
+  FOREIGN KEY (ID_Client) REFERENCES Clients(ID)
+);
+
+CREATE TABLE Vente_Médicament (
+  ID_Vente NUMBER,
+  ID_Médicament NUMBER,
+  Quantité NUMBER,
+  PRIMARY KEY (ID_Vente, ID_Médicament),
+  FOREIGN KEY (ID_Vente) REFERENCES Ventes(ID),
+  FOREIGN KEY (ID_Médicament) REFERENCES Médicaments(ID)
+);
